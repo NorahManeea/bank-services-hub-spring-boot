959e31f4dd93b59eea94f4fdfb3037a891cf55ec
Norah Almaneea <nouraalmanea0@gmail.com>
2024-05-28 06:39:04 +0300

    Account business logic

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -1,15 +1,30 @@
 package com.example.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.accounts.constants.AccountsConstants;
+import com.example.accounts.dto.CustomerDto;
+import com.example.accounts.dto.ResponseDto;
+import com.example.accounts.service.IAccountsService;
+import com.example.accounts.service.impl.AccountsServiceImpl;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/accounts")
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequiredArgsConstructor
 public class AccountsController {
+    private final IAccountsService accountsService;
 
-    @GetMapping
-    public String sayHello() {
-        return "Hello World!";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+        accountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
+
 }
